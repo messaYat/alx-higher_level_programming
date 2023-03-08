be943bd972489079a5032bcd6d99ea4b255174d1
messa <mesfinmetta@gmail.com>
2023-03-08 14:44:38 -0800

    task 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+midle_word  = word[1:-1]
+print(f"First 3 letterss: {word_first_3")
+print(f"Last_2 letters: {word_last_2}")
+print (f"Middle word: {middle word}")
+
