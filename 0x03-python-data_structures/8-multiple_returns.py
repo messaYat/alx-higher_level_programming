ac11ebde523d7392f1aafa75e3967550764b980d
messa <mesfinmetta@gmail.com>
2023-03-12 07:16:40 -0700

    task 8 fixed

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return len(sentence), sentence[0] if sentence else None
-
+    if len(sentence) == 0:
+        my_tuple = (0, None)
+    else:
+        my_tuple = (len(sentence), sentence[:1])
+    return(my_tuple)
