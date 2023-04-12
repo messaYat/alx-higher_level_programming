16d477f9ff2bbe6c110d0e4a028db12ffaaf1378
messa <mesfinmetta@gmail.com>
2023-04-12 11:44:28 -0700

    python inheritance 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """A class with public attribute area"""
+    def area(self):
+        """raise Exception"""
+        raise Exception("area() is not implemented")
