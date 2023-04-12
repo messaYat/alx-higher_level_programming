182885d98c3c9650030a87c136fdc623498cc220
messa <mesfinmetta@gmail.com>
2023-04-12 09:04:36 -0700

    pytho iheritance 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-===========================
-Module with class MyList
-===========================
+subclass from List base class
 """
 
 
 class MyList(list):
-    """Class with method print_sorted"""
-    pass
+    """a subclass of list base class"""
+    def __init__(self):
+        """object constructor"""
+        super().__init__()
+        """Access the methods of the Base class"""
 
     def print_sorted(self):
-        """Methot that sorted a list"""
-
-        print(sorted(list(self)))
+        """print_sorted method"""
+        print(sorted(self))
