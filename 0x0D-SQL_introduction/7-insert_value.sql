56e90dedb48267474fa67fb325153b51429db0a3
messa <mesfinmetta@gmail.com>
2023-05-19 12:23:24 -0700

    my sqli 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- Inserts a new row in the table first_table (current database).
-
+-- Inserts a new row into the table first_table.
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
