5f47c9188c21cb3955dcaa2ed912ff6b79de7f47
messa <mesfinmetta@gmail.com>
2023-06-13 14:25:35 -0700

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,18 @@
-!/usr/bin/node
-// JS script to define a class, creates an instance method to print rectangle
-class Rectangle {
+#!/usr/bin/node
+
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return this;
-    } else {
-      this.width = w;
-      this.height = h;
+    if (w <= 0 || w === undefined || h <= 0 || h === undefined) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print (c) {
+    c = 'X';
+    for (let y = 0; y < this.height; y++) {
+      console.log(c.repeat(this.width));
     }
   }
-  print () {
-    for (let count = 0; count < this.height; count++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-}
-module.exports = Rectangle;
+};
